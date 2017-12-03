e6205e1150cb888ac3ebf8bae7f6971228f05a18
Yash <yash592@gmail.com>
2017-12-03 11:50:09 -0800

    server

---
+++ app/data/friends.js
@@ -0,0 +1,93 @@
+var cats = [
+
+{
+	"name": "Garfield",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Lenny",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Bruce",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Kittu",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Pingu",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Wiz",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Ecko",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Caramel",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Kiki",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+{
+	"name": "Lad",
+	"photo": "",
+	"scores": [
+
+	]
+
+},
+
+]
---
+++ server.js
@@ -0,0 +1,26 @@
+// Depemndencies
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Express config
+
+var app = express();
+
+// Setting up the port for heroku
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// routes
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listeners
+
+app.listen(PORT, function() {
+	console.log("Listening on PORT " + PORT);
+});
